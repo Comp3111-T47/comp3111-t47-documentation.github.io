14007fb35c9b2ddfc2c9e10ea9cbcb34af3ca992
Nick Yeung <phyeungad@ust.hk>
2021-11-28 14:29:25 +0800

    1st commit

---
+++ type-search-index.js
@@ -0,0 +1,5 @@
+<<<<<<< Updated upstream
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"comp3111.covid","l":"Controller"},{"p":"comp3111.covid","l":"TableController"},{"p":"comp3111.covid","l":"TableDataAnalysis"},{"p":"comp3111.covid","l":"TableViewInternal"}];updateSearchResults();
+=======
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"comp3111.covid","l":"TableAView"}];updateSearchResults();
+>>>>>>> Stashed changes
